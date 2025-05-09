6756adfaf61697dc0338d93f3ee9144953a51565
gm-ebs-ext <177268999+gm-ebs-ext@users.noreply.github.com>
2025-05-09 13:45:58 +0100

    remove mock_labels from test-data_prep

--- tests/testthat/test-data_prep.R
+++ tests/testthat/test-data_prep.R
@@ -1,6 +1,5 @@
 test_that("pt_info_data_filter preserves data.frame labels", {
   df <- as.data.frame(safetyData::adam_adsl)
-  mock_labels <- c(LETTERS, letters)
   expected_labels <- get_labels(df)
   output <- pt_info_data_filter(
     df, subjid_var = "USUBJID", columns = names(df), selected_key = "01-701-1015"
