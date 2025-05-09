e46e9923e222c4b5762542a87c939019d74c3677
gm-ebs-ext <177268999+gm-ebs-ext@users.noreply.github.com>
2025-05-09 13:42:07 +0100

    remove setting labels in test-data_prep

--- tests/testthat/test-data_prep.R
+++ tests/testthat/test-data_prep.R
@@ -1,18 +1,9 @@
 test_that("pt_info_data_filter preserves data.frame labels", {
-
   df <- as.data.frame(safetyData::adam_adsl)
   mock_labels <- c(LETTERS, letters)
-
-  for (i in seq_len(ncol(df))) {
-    attr(df[[i]], "label") <- mock_labels[i]
-  }
-
   expected_labels <- get_labels(df)
-
   output <- pt_info_data_filter(
     df, subjid_var = "USUBJID", columns = names(df), selected_key = "01-701-1015"
   )
-
   testthat::expect_identical(get_labels(output), expected_labels)
-
 })
