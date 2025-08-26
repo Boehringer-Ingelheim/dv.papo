95c62402e52283f6c0734d224496a338d3c6b5c6
Matthias,Korbinian (MED BDS) BIP-DE-B <korbinian.matthias@boehringer-ingelheim.com>
2025-08-26 14:28:37 +0100

    add tests

--- tests/testthat/test-data_prep.R
+++ tests/testthat/test-data_prep.R
@@ -6,3 +6,12 @@ test_that("pt_info_data_filter preserves data.frame labels", {
   )
   testthat::expect_identical(get_labels(output), expected_labels)
 })
+
+test_that("pt_info_data_filter also works if only one column is specified", {
+  df <- as.data.frame(safetyData::adam_adsl)
+  testthat::expect_no_error(
+    pt_info_data_filter(
+    df, subjid_var = "USUBJID", columns = "Age", selected_key = "01-701-1015"
+  )
+  )
+})
